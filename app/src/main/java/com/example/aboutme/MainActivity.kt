f689706b45ad6bccba35ea4afc93898feb5b7a68
Tijil <tijil2008@gmail.com>
2021-07-14 07:56:27 +0530

    Added Code In Kotlin File to make and finish the app.  Now I will work on polishing the app and making it smoother. Also added done button

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+ *     * ScrollView to display scrollable text
+ *     * Getting user input with an EditText.
+ *     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
+ *     * Setting the visibility status of a view.
+ *     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+ *       and how to display data in views using the data binding object.
+ */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
     }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
