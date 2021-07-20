e6b12fbcfcc172b7fa0f74aacca18c6eaa34fc5f
Tijil <tijil2008@gmail.com>
2021-07-20 08:00:46 +0530

    Made a new .kt file for instantiating the data binding. Also added the respective name and nickname to MainActivity.kt and activity_main.xml

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -15,11 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Tijil Agrawal")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.myName = myName
         //findViewById<Button>(R.id.done_button).setOnClickListener {
            // addNickname(it)
         //}
@@ -31,7 +34,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text
+            //nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
